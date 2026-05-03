010de7d17ac10c8d92625f36c16bb7df60e626fc
Vitor Soares Pereira <vitors.usp@gmail.com>
2026-05-02 22:39:07 -0300

    Update next.config.js to ignore TypeScript and ESLint errors
    Enable TypeScript build error ignoring and ESLint ignoring during builds.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    optimizePackageImports: ["@vercel/analytics"],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
